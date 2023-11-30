bacddd340ec33af686e322bd45dfbba1f190c85a
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-11-30 23:39:11 +0900

    Add scripts associate with No.7

---
+++ src/1_7_2.py
@@ -0,0 +1,4 @@
+flavor_list = ["vanilla", "chocolate", "pecan", "strawberry"]
+
+for flavor in flavor_list:
+    print(f"{flavor} is delicious")
---
+++ src/1_7_4.py
@@ -0,0 +1,4 @@
+flavor_list = ["vanilla", "chocolate", "pecan", "strawberry"]
+
+for i, flavor in enumerate(flavor_list, 1):
+    print(f"{i}: {flavor}")
